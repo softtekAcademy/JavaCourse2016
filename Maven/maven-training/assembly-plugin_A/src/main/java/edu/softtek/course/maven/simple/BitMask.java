daeb728ca3dea46a545396fb068924483945c1ae
Benjamin Concha <benjamin.concha@staples.com>
2017-01-09 05:26:07 -0600

    Opening Maven's Course, first commit

---
+++ Maven/maven-training/assembly-plugin_A/src/main/java/edu/softtek/course/maven/simple/BitMask.java
@@ -0,0 +1,42 @@
+package edu.softtek.course.maven.simple;
+
+import org.apache.log4j.Logger;
+
+
+public class BitMask {
+
+    private static Logger LOGGER = Logger.getLogger(BitMask.class);
+
+    private long bitMaskValue;
+
+    public long getBitMaskValue() {
+        return bitMaskValue;
+    }
+
+    public BitMask() {
+        bitMaskValue = 0;
+    }
+
+    public BitMask(long currentValue) {
+        this.bitMaskValue = currentValue;
+    }
+
+    public void setBit(int bit) {
+        long bitMask = Long.rotateLeft(1, bit);
+        bitMaskValue |= bitMask;
+    }
+
+    public void unsetBit(int bit) {
+        long bitMask = Long.rotateLeft(1, bit);
+        bitMaskValue ^= bitMask;
+    }
+
+    public boolean isBitSet(int bit) {
+        long bitMask = Long.rotateLeft(1, bit);
+        long result = this.bitMaskValue & bitMask;
+        if (result != 0) {
+            return true;
+        }
+        return false;
+    }
+}
